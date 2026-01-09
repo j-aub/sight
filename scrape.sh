dbb2565554e956f3aca4f8b599ec80cd1387d11c
j-aub <j-aub@users.noreply.github.com>
2026-01-09 18:09:40 -0500

    grant write permission to github actions bot

--- scrape.sh
+++ scrape.sh
@@ -5,12 +5,6 @@ set -eux
 uv run scrape.py
 
 # commit changes
-# https://stackoverflow.com/questions/69839851/github-actions-copy-git-user-name-and-user-email-from-last-commit
-# https://docs.github.com/en/account-and-profile/how-tos/email-preferences/setting-your-commit-email-address#about-commit-email-addresses
-NAME="${GITHUB_ACTOR:-octocat}"
-EMAIL="${GITHUB_ACTOR_ID:-101839405}+${NAME}@users.noreply.github.com"
-git config user.name "$NAME"
-git config user.email "$EMAIL"
 git add output
 git commit -m 'Update data'
 git push
